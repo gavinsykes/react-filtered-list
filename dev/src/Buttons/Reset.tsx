f43df37ac22e26493c4f4ad050b2e425acd07b8c
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2023-12-10 22:56:13 +0000

    Adding custom event handler

--- dev/src/Buttons/Reset.tsx
+++ dev/src/Buttons/Reset.tsx
@@ -1,18 +1,24 @@
-import { HTMLProps } from 'react';
+import { HTMLProps, MouseEventHandler } from 'react';
 import { useFilteredListContext } from '../FilteredList';
 
 interface FilteredListResetFiltersButtonProps
   extends HTMLProps<HTMLButtonElement> {
   children: string;
+  onReset?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function FilteredListResetFiltersButton({
   children,
+  onReset,
   ...buttonProps
 }: FilteredListResetFiltersButtonProps) {
   const { resetFilters } = useFilteredListContext();
+  const handleClick: MouseEventHandler<HTMLButtonElement> = async e => {
+    resetFilters();
+    if (onReset) onReset(e)
+  }
   return (
-    <button {...buttonProps} type="button" onClick={resetFilters}>
+    <button {...buttonProps} type="button" onClick={handleClick}>
       {children}
     </button>
   );
